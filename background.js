c3ea332622e5d17acaf49b36307acbd2a05a1f71
AccCreate <dhseo1011@gmail.com>
2018-01-20 15:43:55 -0500

    Date in MLA format

--- background.js
+++ background.js
@@ -5,16 +5,17 @@ var highlights = {};
 // text = {html link, List of quotes}
 // A function to use as callback
 function report_citations(data) {
-	if (data["url"] in highlights) {
-		highlights[data["url"]].push(data["text"]);
-	} else {
-		highlights[data["url"]] = [data["text"]];
-	}
-	alert(highlights[data["url"]]);
+    
+    if (data["url"] in highlights) {
+	highlights[data["url"]].push(data["text"]);
+    } else {
+	highlights[data["url"]] = [data["text"]];
+    }
+    alert(highlights[data["url"]]);
 }
 
 // When the browser-action button is clicked...
 chrome.browserAction.onClicked.addListener(function (tab) {
-	// ...if it matches, send a message specifying a callback too
-	chrome.tabs.sendMessage(tab.id, {text: 'ACK'}, report_citations);
+    // ...if it matches, send a message specifying a callback too
+    chrome.tabs.sendMessage(tab.id, {text: 'ACK'}, report_citations);
 });
