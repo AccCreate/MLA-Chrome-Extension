ad50a8be07cea68646ce38adf4a472a446eca9ec
AccCreate <dhseo1011@gmail.com>
2018-01-22 11:01:34 -0500

    Finds time of most websites. Only improvement really is the checking from @schema stuffs if possible (optional)

--- content.js
+++ content.js
@@ -62,19 +62,32 @@ function getArticleDate(docHeadHTML, docBodyHTML){
     var maxArticleDate = new Date(document.lastModified);
     var validDateList = [];
 
-    // </span> <span class="pb-timestamp" itemprop="datePublished" content="2018-01-20T09:45-500">January 20 at 9:45 PM</span>
     // Finding from "span"
+    // </span> <span class="pb-timestamp" itemprop="datePublished" content="2018-01-20T09:45-500">January 20 at 9:45 PM</span>
     // var str = "</span> <span class=\"pb-timestamp\" itemprop=\"datePublished\" content=\"2018-01-20T09:45-500\">January 20 at 9:45 PM</span>";
     // <span class="pb-timestamp" itemprop="datePublished" content="2018-01-21T04:51-500">January 21 at 4:51 PM</span> <span clas0s="pb-tool email">
-    var spanDateList = [];
-    // var dateList = document.querySelectorAll("span 'dat' >");
-    
     // <p class="update-time">Updated 9:55 PM ET, Sun January 21, 2018
     // Finding from "class"
+    var spanDateList = [];
+    var classDateList = document.querySelectorAll('[class*="time" i]');
+    for(i = 0; i < classDateList.length; i++) {
+	if(classDateList[i].getAttribute("content") != undefined){
+	    var tempDate2 = classDateList[i].getAttribute("content");
+	    tempDate2 = (tempDate2.replace(/[&\/\\#+()$~%.'"*?<>{}]/g, '')).trim();
+	    if (tempDate2 != ""){
+		spanDateList.push(tempDate2);
+	    }
+	}
+    }
+    for(i = 0; i < spanDateList.length; i++) {
+	appendValidDate(spanDateList[i], validDateList, maxArticleDate);
+    }
     
 
+    // Finding from {"@context schema", etc. for mobile checking
     // {"@context":"http:\/\/schema.org","
-    
+
+    // Finding from <time> tag
     // <time datetime="2017-05-09T17:00:00+02:00" itemprop="datePublished">May 9, 2017</time>
     // <time datetime="2016-10-03T15:15:18.819Z">Oct 3, 2016</time>
     // Finding from "time" itself
@@ -87,23 +100,21 @@ function getArticleDate(docHeadHTML, docBodyHTML){
     // <meta name="DISPLAYDATE" content="Jan. 21, 2018" />
     // <meta name="dat" content="Jan. 21, 2018" />
     var metaDateList = [];
-    dateList = document.querySelectorAll("meta[name*='dat' i");
+    var dateList = document.querySelectorAll("meta[name*='dat' i");
     // alert(document.querySelector("meta[name='"+"dat"+"']").getAttribute('content'));
     for(i = 0; i < dateList.length; i ++){
 	if(dateList[i].getAttribute("content") != undefined){
 	    var tempDate = dateList[i].getAttribute("content");
-	    tempDate = (tempDate.replace(/[&\/\\#+()$~%.'":*?<>{}]/g, '')).trim();
+	    tempDate = (tempDate.replace(/[&\/\\#+()$~%.'"*?<>{}]/g, '')).trim();
 	    if (tempDate != ""){
-		metaDateList.push(dateList[i].getAttribute("content"));
+		metaDateList.push(tempDate);
 	    }
 	    
 	}
     }
-
     for(i = 0; i < metaDateList.length; i ++){
 	appendValidDate(metaDateList[i], validDateList, maxArticleDate);
     }	
-    
     
     if(validDateList.length > 0){
 	return (articleDate = getMostFrequentElement(validDateList));
@@ -144,11 +155,16 @@ function appendValidDate(eachDate, validDateList, maxArticleDate){
     // 2nd, 3rd, 4th, etc. for day that needs to be removed
     eachDate = eachDate.replace(/(\d+)(st|nd|rd|th)/, "$1");
 
+    // 2018-01-22T08:00-500. Remove "T" out in this case
+    eachDate = eachDate.replace(/(\d+)(T|t)/, "$1 ");
     var regYYMMDD = /[1-2][0-9]{3}[0-1][0-9][0-3][0-9]/;
+
+    // Change to YY-MM-DD if YYMMDD
     if(regYYMMDD.test(eachDate)){
 	eachDate = eachDate.replace(/(\d{4})(\d{2})(\d{2})/g, '$1-$2-$3 '); 
     }
 
+    // Check if Date valid. If it is, append it
     var validDateCheck = new Date(eachDate);
     if(validDateCheck instanceof Date && !isNaN(validDateCheck.valueOf())){
 	if(maxArticleDate >= validDateCheck){
