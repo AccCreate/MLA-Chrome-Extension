604b9b4eae861defc9d56c04b9c0c12c16e17dce
AccCreate <dhseo1011@gmail.com>
2018-01-22 08:30:25 -0500

    Meta for Date mostly working (could be 1 day off due to time zone for now)

--- content.js
+++ content.js
@@ -9,7 +9,10 @@ chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
 	var docHeadHTML = document.head.innerHTML;
 	var docBodyHTML = document.body.innerHTML;
 
-	alert(docHTML);
+	// NOTE: Getting tags through "getElementByTagName" fails at large HTML files
+	// After around 125 metas, it stops finding more so DO NOT use this.
+	// var allMETATag = document.getElementsByTagName("meta");
+
 	// store selected text
 	var text = "";
 
@@ -22,41 +25,21 @@ chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
 	
 
 	// itemprop="author" or itemprop="name"
-	var author = "";
-	var other = document.getElementsByTagName('li');
-	alert(document.body.innerHTML);
-	for (var i=0;i<other.length;i++) {
-	    alert(other[i].innerHTML);
-	    if (other[i].className.toLowerCase()=='author'){
-		author=other[i].getElementsByTagName('a')[0].innerHTML;
+	// var author = "";
+	// var other = document.getElementsByTagName('li');
+	// alert(document.body.innerHTML);
+	// for (var i=0;i<other.length;i++) {
+	//     alert(other[i].innerHTML);
+	//     if (other[i].className.toLowerCase()=='author'){
+	// 	author=other[i].getElementsByTagName('a')[0].innerHTML;
 
-	    }
-	}
-	alert(author);
-
-	// Get date article was published
-	// <p class="update-time">Updated 9:55 PM ET, Sun January 21, 2018
-	// 	<meta name="DISPLAYDATE" content="Jan. 21, 2018" />
-	// <meta name="dat" content="Jan. 21, 2018" />
-	// </span> <span class="pb-timestamp" itemprop="datePublished" content="2018-01-20T09:45-500">January 20 at 9:45 PM</span>
-	// <time datetime="2017-05-09T17:00:00+02:00" itemprop="datePublished">May 9, 2017</time>
-	var articleDate = "";
-
-	// Finding from any "meta" page
-	var str = "<meta name=\"dat\" content=\"Jan. 21, 2018\" />";
-	var n = str.search(/<meta name="[a-z]*dat[a-z]*" content="(.*?)"/i);
-	var newtext = str.replace(/(<meta name="[a-z]*dat[a-z]*") (content=")(.*?)(")/i, "$3");
-	newtext = newtext.replace(/[!@#$%^&*/>]/g, "");
-	newtext = newtext.trim("");
-	// document.getElementById("demo").innerHTML = newtext;
+	//     }
+	// }
+	// alert(author);
 	
-	var index = docHeadHTML.indexOf('date');
-	// https://stackoverflow.com/questions/7998180/regex-how-to-extract-text-from-between-quotes-and-exclude-quotes
-	// /<meta name="dat[a-z]*" content="(.*?)"/i
-	
-	alert(document.lastModified);
-	alert("nopeeeeeeeeeeee");
-	
+	// Get date of article
+	var articleDate = getArticleDate(docHeadHTML, docBodyHTML);
+	alert(articleDate);
 	
 	if (window.getSelection) {
 	    text = window.getSelection().toString();
@@ -68,3 +51,94 @@ chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
 	sendResponse({url: url, text: text, date: date, company: company});
     }
 });
+
+function getArticleDate(docHeadHTML, docBodyHTML){
+    // Get date article was published
+    var articleDate = "";
+    var maxArticleDate = new Date(document.lastModified);
+    var validDateList = [];
+
+    // </span> <span class="pb-timestamp" itemprop="datePublished" content="2018-01-20T09:45-500">January 20 at 9:45 PM</span>
+    // Finding from "span"
+    // var str = "</span> <span class=\"pb-timestamp\" itemprop=\"datePublished\" content=\"2018-01-20T09:45-500\">January 20 at 9:45 PM</span>";
+    // <span class="pb-timestamp" itemprop="datePublished" content="2018-01-21T04:51-500">January 21 at 4:51 PM</span> <span clas0s="pb-tool email">
+    var spanDateList = [];
+    // var dateList = document.querySelectorAll("span 'dat' >");
+    
+    // <p class="update-time">Updated 9:55 PM ET, Sun January 21, 2018
+    // Finding from "class"
+    
+    
+    // <time datetime="2017-05-09T17:00:00+02:00" itemprop="datePublished">May 9, 2017</time>
+    // Finding from "time" itself
+    var timeDateList = [];
+    
+    // Finding from any "meta" page
+    // <meta name="DISPLAYDATE" content="Jan. 21, 2018" />
+    // <meta name="dat" content="Jan. 21, 2018" />
+    var metaDateList = [];
+    dateList = document.querySelectorAll("meta[name*='dat' i");
+    // alert(document.querySelector("meta[name='"+"dat"+"']").getAttribute('content'));
+    for(i = 0; i < dateList.length; i ++){
+	if(dateList[i].getAttribute("content") != undefined){
+	    var tempDate = dateList[i].getAttribute("content");
+	    tempDate = tempDate.replace(/[&\/\\#+()$~%.'":*?<>{}]/g, '');
+	    tempDate = tempDate.trim();
+	    if (tempDate != ""){
+		metaDateList.push(dateList[i].getAttribute("content"));
+	    }
+	    
+	}
+    }
+
+    var regYYMMDD = /[1-2][0-9]{3}[0-1][0-9][0-3][0-9]/;
+    for(i = 0; i < metaDateList.length; i ++){
+	if(regYYMMDD.test(metaDateList[i])){
+	    metaDateList[i] = metaDateList[i].replace(/(\d{4})(\d{2})(\d{2})/g, '$1-$2-$3 '); 
+	}
+
+	var validDateCheck = new Date(metaDateList[i]);
+	if(validDateCheck instanceof Date && !isNaN(validDateCheck.valueOf())){
+		alert(validDateCheck);
+	    if(maxArticleDate >= validDateCheck){
+		validDateCheck = (validDateCheck.toString()).replace(/(\w+)\s(\w+)\s(\d{2})\s(\d{4})\s(\d{2}:\d{2}:\d{2})\s(.*)/g, '$3 $2 $4');
+		validDateList.push(validDateCheck);
+	    }
+	}
+    }
+
+
+    if(validDateList.length > 0){
+	return (articleDate = getMostFrequentElement(validDateList));
+    } else {
+	return "N.a.";
+    }
+ 
+    // var str = "<meta name=\"dat\" content=\"Jan. 21, 2018\" />";
+    // var newtext = str.replace(/(<meta name="[a-z]*dat[a-z]*") (content=")(.*?)(")/i, "$3");
+    // var n = str.search(/<meta name="[a-z]*dat[a-z]*" content="(.*?)"/i);
+    // document.getElementById("demo").innerHTML = newtext;
+}
+
+// https://stackoverflow.com/questions/1053843/get-the-element-with-the-highest-occurrence-in-an-array
+// Does not factor for ties
+function getMostFrequentElement(array){
+    if(array.length == 0)
+	return null;
+    var modeMap = {};
+    var maxEl = array[0], maxCount = 1;
+    for(var i = 0; i < array.length; i++)
+    {
+	var el = array[i];
+	if(modeMap[el] == null)
+	    modeMap[el] = 1;
+	else
+	    modeMap[el]++;  
+	if(modeMap[el] > maxCount)
+	{
+	    maxEl = el;
+	    maxCount = modeMap[el];
+	}
+    }
+    return maxEl;
+}
